19e0f54e752f5aca61bf90b768e9eaf1d5ac4f87
AngeloMari <santosangelomari@gmail.com>
2021-05-31 18:21:17 +0800

    Add project files.

---
+++ Contact Tracer App/Contact Tracing.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace Contact_Tracer_App
+{
+    partial class Contact_Tracing
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtboxName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblContact = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblSubname = new System.Windows.Forms.Label();
+            this.txtboxAddress = new System.Windows.Forms.TextBox();
+            this.lblSubaddress = new System.Windows.Forms.Label();
+            this.txtboxAge = new System.Windows.Forms.TextBox();
+            this.txtboxContact = new System.Windows.Forms.TextBox();
+            this.txtboxEmail = new System.Windows.Forms.TextBox();
+            this.txtboxTemp = new System.Windows.Forms.TextBox();
+            this.txtboxTime = new System.Windows.Forms.TextBox();
+            this.pnlColor = new System.Windows.Forms.Panel();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pnlColor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtboxName
+            // 
+            this.txtboxName.Location = new System.Drawing.Point(70, 13);
+            this.txtboxName.Name = "txtboxName";
+            this.txtboxName.Size = new System.Drawing.Size(256, 23);
+            this.txtboxName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(42, 15);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Location = new System.Drawing.Point(11, 193);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(78, 15);
+            this.lblTemp.TabIndex = 3;
+            this.lblTemp.Text = "Temperature:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(194, 193);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(37, 15);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Time:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(12, 151);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(84, 15);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email Address:";
+            // 
+            // lblContact
+            // 
+            this.lblContact.AutoSize = true;
+            this.lblContact.Location = new System.Drawing.Point(11, 113);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(99, 15);
+            this.lblContact.TabIndex = 6;
+            this.lblContact.Text = "Contact Number:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(349, 16);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(31, 15);
+            this.lblAge.TabIndex = 7;
+            this.lblAge.Text = "Age:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(12, 66);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(52, 15);
+            this.lblAddress.TabIndex = 8;
+            this.lblAddress.Text = "Address:";
+            // 
+            // lblSubname
+            // 
+            this.lblSubname.AutoSize = true;
+            this.lblSubname.Font = new System.Drawing.Font("Segoe UI", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSubname.Location = new System.Drawing.Point(70, 39);
+            this.lblSubname.Name = "lblSubname";
+            this.lblSubname.Size = new System.Drawing.Size(114, 12);
+            this.lblSubname.TabIndex = 9;
+            this.lblSubname.Text = "Surname, First Name M.I.";
+            // 
+            // txtboxAddress
+            // 
+            this.txtboxAddress.Location = new System.Drawing.Point(70, 63);
+            this.txtboxAddress.Name = "txtboxAddress";
+            this.txtboxAddress.Size = new System.Drawing.Size(256, 23);
+            this.txtboxAddress.TabIndex = 10;
+            // 
+            // lblSubaddress
+            // 
+            this.lblSubaddress.AutoSize = true;
+            this.lblSubaddress.Font = new System.Drawing.Font("Segoe UI", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSubaddress.Location = new System.Drawing.Point(70, 89);
+            this.lblSubaddress.Name = "lblSubaddress";
+            this.lblSubaddress.Size = new System.Drawing.Size(138, 12);
+            this.lblSubaddress.TabIndex = 12;
+            this.lblSubaddress.Text = "Blk Lot # Street, Barangay, City";
+            // 
+            // txtboxAge
+            // 
+            this.txtboxAge.Location = new System.Drawing.Point(386, 12);
+            this.txtboxAge.Name = "txtboxAge";
+            this.txtboxAge.Size = new System.Drawing.Size(40, 23);
+            this.txtboxAge.TabIndex = 13;
+            // 
+            // txtboxContact
+            // 
+            this.txtboxContact.Location = new System.Drawing.Point(116, 110);
+            this.txtboxContact.Name = "txtboxContact";
+            this.txtboxContact.Size = new System.Drawing.Size(210, 23);
+            this.txtboxContact.TabIndex = 14;
+            // 
+            // txtboxEmail
+            // 
+            this.txtboxEmail.Location = new System.Drawing.Point(116, 148);
+            this.txtboxEmail.Name = "txtboxEmail";
+            this.txtboxEmail.Size = new System.Drawing.Size(210, 23);
+            this.txtboxEmail.TabIndex = 15;
+            // 
+            // txtboxTemp
+            // 
+            this.txtboxTemp.Location = new System.Drawing.Point(95, 190);
+            this.txtboxTemp.Name = "txtboxTemp";
+            this.txtboxTemp.Size = new System.Drawing.Size(89, 23);
+            this.txtboxTemp.TabIndex = 16;
+            // 
+            // txtboxTime
+            // 
+            this.txtboxTime.Location = new System.Drawing.Point(237, 190);
+            this.txtboxTime.Name = "txtboxTime";
+            this.txtboxTime.Size = new System.Drawing.Size(89, 23);
+            this.txtboxTime.TabIndex = 17;
+            // 
+            // pnlColor
+            // 
+            this.pnlColor.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlColor.Controls.Add(this.btnEdit);
+            this.pnlColor.Controls.Add(this.btnSave);
+            this.pnlColor.Location = new System.Drawing.Point(348, 148);
+            this.pnlColor.Name = "pnlColor";
+            this.pnlColor.Size = new System.Drawing.Size(82, 65);
+            this.pnlColor.TabIndex = 18;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(3, 35);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(4, 8);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(74, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Contact_Tracing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 231);
+            this.Controls.Add(this.pnlColor);
+            this.Controls.Add(this.txtboxTime);
+            this.Controls.Add(this.txtboxTemp);
+            this.Controls.Add(this.txtboxEmail);
+            this.Controls.Add(this.txtboxContact);
+            this.Controls.Add(this.txtboxAge);
+            this.Controls.Add(this.lblSubaddress);
+            this.Controls.Add(this.txtboxAddress);
+            this.Controls.Add(this.lblSubname);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblContact);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtboxName);
+            this.Name = "Contact_Tracing";
+            this.Text = "Contact Tracing";
+            this.pnlColor.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtboxName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblContact;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblSubname;
+        private System.Windows.Forms.TextBox txtboxAddress;
+        private System.Windows.Forms.Label lblSubaddress;
+        private System.Windows.Forms.TextBox txtboxAge;
+        private System.Windows.Forms.TextBox txtboxContact;
+        private System.Windows.Forms.TextBox txtboxEmail;
+        private System.Windows.Forms.TextBox txtboxTemp;
+        private System.Windows.Forms.TextBox txtboxTime;
+        private System.Windows.Forms.Panel pnlColor;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Contact Tracer App/Contact Tracing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Tracer_App
+{
+    public partial class Contact_Tracing : Form
+    {
+        public Contact_Tracing()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
