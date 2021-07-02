d1c62e9b160ff5ecd99e6396dcfa1475cf4c1a8d
AngeloMari <santosangelomari@gmail.com>
2021-07-02 18:04:50 +0800

    Converted to OOP.

--- Contact Tracer App/Contact Tracing.cs
+++ Contact Tracer App/Contact Tracing.cs
@@ -13,6 +13,7 @@ namespace Contact_Tracer_App
 {
     public partial class Contact_Tracing : Form
     {
+        inputParameters inputConditions = new();
         public Contact_Tracing()
         {
             InitializeComponent();
@@ -20,7 +21,46 @@ namespace Contact_Tracer_App
 
         private void btnSave_Click(object sender, EventArgs e)
         {//To require all fields to be filled properly before writing a file.
-            inputParameter();
+            inputConditions.Address = txtboxAddress.Text;
+            inputConditions.Age = txtboxAge.Text;
+            inputConditions.Contact = txtboxContact.Text;
+            inputConditions.Email = txtboxEmail.Text;
+            inputConditions.Name = txtboxName.Text;
+            inputConditions.Temp = txtboxTemp.Text;
+            inputConditions.Hour = cmbboxHour.Text;
+            inputConditions.Minute = cmbboxMinute.Text;
+            inputConditions.AMPM = cmbboxAMPM.Text;
+            inputConditions.Parameters();
+            txtboxAddress.Text = inputConditions.Address;
+            txtboxAge.Text = inputConditions.Age;
+            txtboxContact.Text = inputConditions.Contact;
+            txtboxEmail.Text = inputConditions.Email;
+            txtboxName.Text = inputConditions.Name;
+            txtboxTemp.Text = inputConditions.Temp;
+            cmbboxHour.Text = inputConditions.Hour;
+            cmbboxMinute.Text = inputConditions.Minute;
+            cmbboxAMPM.Text = inputConditions.AMPM;
+
+            if (!inputConditions.parameterPassed)
+            {
+                btnRead.Hide();
+                MessageBox.Show(inputConditions.ErrorMessage);
+            }
+            else
+            {
+                btnSave.Hide();
+                btnRead.Show();
+
+                txtboxAddress.Enabled = false;
+                txtboxAge.Enabled = false;
+                txtboxContact.Enabled = false;
+                txtboxEmail.Enabled = false;
+                txtboxName.Enabled = false;
+                txtboxTemp.Enabled = false;
+                cmbboxAMPM.Enabled = false;
+                cmbboxHour.Enabled = false;
+                cmbboxMinute.Enabled = false;
+            }
         }
 
         private void btnRead_Click(object sender, EventArgs e)
@@ -61,118 +101,6 @@ namespace Contact_Tracer_App
             traceFile.WriteLine("You can find the contact tracing details here.");
             traceFile.WriteLine("Each file with the date as a name contains the information for each day.");
             traceFile.Close();
-        }
-
-        private void inputParameter()
-        {
-            if (string.IsNullOrEmpty(txtboxAddress.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (string.IsNullOrEmpty(txtboxAge.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (string.IsNullOrEmpty(txtboxContact.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (string.IsNullOrEmpty(txtboxEmail.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (!txtboxEmail.Text.Contains("@"))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (string.IsNullOrEmpty(txtboxName.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (string.IsNullOrEmpty(txtboxTemp.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (string.IsNullOrEmpty(cmbboxHour.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (string.IsNullOrEmpty(cmbboxMinute.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (string.IsNullOrEmpty(cmbboxAMPM.Text))
-            {
-                btnRead.Hide();
-                MessageBox.Show("Please answer all fields!");
-            }
-            else if (short.TryParse(txtboxAge.Text, out short age) && ((age < 18) || (age > 65))) //To check the appropriate age
-            {
-                btnRead.Hide();
-                btnSave.Hide();
-                MessageBox.Show("Sorry, you are not allowed to go outside based on your age.");
-            }
-            else if (!short.TryParse(txtboxAge.Text, out short ageNum))
-            {//To not allow the user from entering letters or word
-                btnRead.Hide();
-                btnSave.Hide();
-                MessageBox.Show("Please enter a number on the Age textbox!");
-            }
-            else if (float.TryParse(txtboxTemp.Text, out float temp) && (temp >= 37.4)) //To check the appropriate body temperature.
-            {
-                btnRead.Hide();
-                btnSave.Hide();
-                MessageBox.Show("Sorry, you should not be going outside." +
-                    "\nYour body temperature is " + temp + "°C" + "." + "\nYou have a fever!");
-            }
-            else if (!float.TryParse(txtboxTemp.Text, out float tempNum))
-            {
-                btnRead.Hide();
-                btnSave.Hide();
-                MessageBox.Show("Please enter a number on the Temperature textbox!");
-            }
-            //If everything is working accordingly, the program will now write the file.
-            else
-            {
-                StreamWriter traceFile;
-                string createDate = DateTime.UtcNow.ToLongDateString() + ".txt";
-                string date = DateTime.UtcNow.ToLongDateString();
-
-                traceFile = File.AppendText(createDate);
-                traceFile.WriteLine("Name: " + txtboxName.Text);
-                traceFile.WriteLine("Age: " + txtboxAge.Text);
-                traceFile.WriteLine("Address: " + txtboxAddress.Text);
-                traceFile.WriteLine("Contact No.: " + txtboxContact.Text);
-                traceFile.WriteLine("Email: " + txtboxEmail.Text);
-                traceFile.WriteLine("Temperature: " + txtboxTemp.Text + "°C");
-                traceFile.WriteLine("Time: " + cmbboxHour.Text + ":" + cmbboxMinute.Text + " " + cmbboxAMPM.Text);
-                traceFile.WriteLine("");
-                traceFile.WriteLine("_______________________________________________");
-                traceFile.WriteLine("");
-                traceFile.Close();
-
-                btnRead.Show();
-                btnSave.Hide();
-
-                txtboxAddress.Enabled = false;
-                txtboxAge.Enabled = false;
-                txtboxContact.Enabled = false;
-                txtboxEmail.Enabled = false;
-                txtboxName.Enabled = false;
-                txtboxTemp.Enabled = false;
-                cmbboxAMPM.Enabled = false;
-                cmbboxHour.Enabled = false;
-                cmbboxMinute.Enabled = false;
-            }
         }
     }
 }
---
+++ Contact Tracer App/inputParameters.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Tracer_App
+{
+    class inputParameters
+    {
+        public string Address = "";
+        public string Age = "";
+        public string Contact = "";
+        public string Email = "";
+        public string Name = "";
+        public string Temp = "";
+        public string Hour = "";
+        public string Minute = "";
+        public string AMPM = "";
+        public string ErrorMessage = "";
+        public bool parameterPassed = true;
+
+        public void Parameters()
+        {
+            if (string.IsNullOrEmpty(Address))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (string.IsNullOrEmpty(Age))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (string.IsNullOrEmpty(Contact))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (string.IsNullOrEmpty(Email))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (!Email.Contains("@"))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (string.IsNullOrEmpty(Name))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (string.IsNullOrEmpty(Temp))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (string.IsNullOrEmpty(Hour))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (string.IsNullOrEmpty(Minute))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (string.IsNullOrEmpty(AMPM))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please answer all fields!";
+            }
+            else if (short.TryParse(Age, out short age) && ((age < 18) || (age > 65))) //To check the appropriate age
+            {
+                parameterPassed = false;
+                ErrorMessage = "Sorry, you are not allowed to go outside based on your age.";
+            }
+            else if (!short.TryParse(Age, out short ageNum))
+            {//To not allow the user from entering letters or word
+                parameterPassed = false;
+                ErrorMessage = "Please enter a number on the Age textbox!";
+            }
+            else if (float.TryParse(Temp, out float temp) && (temp >= 37.4)) //To check the appropriate body temperature.
+            {
+                parameterPassed = false;
+                ErrorMessage = "Sorry, you should not be going outside." +
+                    "\nYour body temperature is " + temp + "°C" + "." + "\nYou have a fever!";
+            }
+            else if (!float.TryParse(Temp, out float tempNum))
+            {
+                parameterPassed = false;
+                ErrorMessage = "Please enter a number on the Temperature textbox!";
+            }
+            //If everything is working accordingly, the program will now write the file.
+            else
+            {
+                parameterPassed = true;
+
+                StreamWriter traceFile;
+                string createDate = DateTime.UtcNow.ToLongDateString() + ".txt";
+                string date = DateTime.UtcNow.ToLongDateString();
+
+                traceFile = File.AppendText(createDate);
+                traceFile.WriteLine("Name: " + Name);
+                traceFile.WriteLine("Age: " + Age);
+                traceFile.WriteLine("Address: " + Address);
+                traceFile.WriteLine("Contact No.: " + Contact);
+                traceFile.WriteLine("Email: " + Email);
+                traceFile.WriteLine("Temperature: " + Temp + "°C");
+                traceFile.WriteLine("Time: " + Hour + ":" + Minute + " " + AMPM);
+                traceFile.WriteLine("");
+                traceFile.WriteLine("_______________________________________________");
+                traceFile.WriteLine("");
+                traceFile.Close();
+            }
+        }
+    }
+}
